f52e8b70641392b92b465ba08b75ec823e8c3aae
het <me@het.im>
2016-08-18 19:39:04 +0800

    fix gradle wrapper problem

--- sample/src/main/java/im/het/tiny/sample/MainActivity.java
+++ sample/src/main/java/im/het/tiny/sample/MainActivity.java
@@ -1,6 +1,13 @@
 package im.het.tiny.sample;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 }
